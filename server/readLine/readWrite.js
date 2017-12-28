6c079e8cd7f0a0f56a3c8a8c66e09b9829d3853a
JosephJvB <jo-van-bo@hotmail.com>
2017-12-29 10:27:57 +1300

    can write to add times

--- server/readLine/readWrite.js
+++ server/readLine/readWrite.js
@@ -1,16 +1,32 @@
 const fs = require('fs')
 const path = require('path')
 
-readWrite()
-
-function readWrite () {
+function newRun (num) {
   fs.readFile(path.join(__dirname, 'recordTemplate.json'), 'utf8', (err, data) => {
     if (err) console.log(err)
     else {
-      fs.writeFile(`${__dirname}/../records/test.json`, data, err => {
+      fs.writeFile(`${__dirname}/../records/testrun${num}.json`, data, err => {
         if (err) console.log(err)
-        else console.log('nice one', JSON.parse(data))
+        else console.log('Good luck!')
       })
     }
   })
 }
+
+logTime('../records/testrun6.json', 'E2', 'M1', 9)
+
+function logTime (path, e, m, time) {
+  fs.readFile(`${__dirname}/${path}`, 'utf8', (err, data) => {
+    if (err) console.log(err)
+    else {
+      let newTime = JSON.parse(data)
+      newTime[e][m] = time
+      fs.writeFile(`${__dirname}/${path}`, JSON.stringify(newTime), err => {
+        if (err) console.log(err)
+        else console.log(newTime)
+      })
+    }
+  })
+}
+
+module.exports = { newRun }
